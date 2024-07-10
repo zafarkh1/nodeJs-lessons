aaae6a7eba455a1228e9ac3cea175d62d6c17896
Zafar <hatamovzafar7070@gmail.com>
2024-07-10 23:11:25 +0500

    'learning'

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// Lesson-2
+//                                                       Introduction
 
-console.log() //gloabal
+// function sayHello (name) {
+//     console.log('Hello ' + name)
+// }
+//
+// sayHello('Zafar')
+
+//                                                           Global object
+
+// console.log() //gloabal
 
 // console.log(window) //there is no window object in node
-console.log(global) //instead we have global object
-
-var msg = 'Hello world'
-console.log(global.msg) //undefined, because msg variable is available only in this (app.js) file not outside of browser
+// console.log(global) //instead we have global object
+//
+// var msg = 'Hello world'
+// console.log(global.msg) //undefined, because msg variable is available only in this (app.js) file not outside of browser
