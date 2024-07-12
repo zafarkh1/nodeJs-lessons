9195229d6b72896f105c67104869882f2c0ff92b
Zafar <hatamovzafar7070@gmail.com>
2024-07-12 15:48:25 +0500

    npm_finished

--- app.js
+++
@@ -1,9 +0,0 @@
-// Lesson-2
-
-console.log() //gloabal
-
-// console.log(window) //there is no window object in node
-console.log(global) //instead we have global object
-
-var msg = 'Hello world'
-console.log(global.msg) //undefined, because msg variable is available only in this (app.js) file not outside of browser
