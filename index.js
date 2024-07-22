3f8cd11477d5a5b42009f690bd20e27030c6e1e1
Zafar <hatamovzafar7070@gmail.com>
2024-07-22 13:58:50 +0500

    pagination_work

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //                               Connecting to mongodb
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Connected to DB'))
   .catch(err => console.error('Error connecting to DB', err));
 
@@ -31,18 +34,29 @@ async function createCourse() {
 
 //                              Querying documents
 async function getCourses() {
+  const pageNumber = 2
+  const pageSize = 10
   const courses = await Course
     // .find({author: 'Zafar'})
-  //                            Comparison operators
+    //                            Comparison operators
     // .find({price: {$gte: 10, $lte: 20}})
     // .find({price: {$in: [10, 15, 20]}})
     //                           Logical operators
-    .find()
-    .or([{author: 'Zafar'}, {isPublished: true}])
-    .and([{ author: 'Zafar' }, {isPublished: true}])
+    // .find()
+    // .or([{author: 'Zafar'}, {isPublished: true}])
+    // .and([{ author: 'Zafar' }, {isPublished: true}])
+    //                           Regular expression
+    // .find({author: /^Zafar/}) // Starts with Zafar
+    // .find({author: /Doe/i}) // Ends with Doe and case-insensitive
+    .find({author: /.*Zafar.*/i}) // Contains Zafar
     .sort({name: -1})
-    .limit(5)
-    .select({name: 1, tag: 1})
+    // .select({name: 1, tag: 1})
+    //                            Counting
+    .countDocuments()
+    //                            Pagination
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+  // .limit(1)
   console.log(courses)
 }
 
